459e48dfb68b17b6ad8d92c97becded837555d36
Tunnes <cauepani06@gmail.com>
2023-03-02 08:48:13 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,10 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/Design-System-Ignite2023/'
+    }
   }
 }
